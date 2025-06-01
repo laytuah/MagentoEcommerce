7d12aa6abc14771d73ab46b2b38be4bad1959533
laytuah <olasupoa.o@gmail.com>
2025-06-01 23:39:21 +0100

    refactored

--- MagentoEcommerce/Utilities/PageElement.cs
+++ MagentoEcommerce/Utilities/PageElement.cs
@@ -6,29 +6,12 @@ using SeleniumExtras.WaitHelpers;
 using System.Collections.ObjectModel;
 using System.Drawing;
 
-public class PageElement : IWebElement
+public class PageElement
 {
-    IWebDriver _driver;
-    By _locator;
-    WebDriverWait _wait;
-    ReadOnlyCollection<IWebElement> elements => _driver.FindElements(_locator);
-    TimeSpan _timeout = TimeSpan.FromSeconds(20);
-
-    IWebElement _element
-    {
-        get
-        {
-            try
-            {
-                return _driver.FindElement(_locator);
-            }
-            catch (StaleElementReferenceException)
-            {
-                return _driver.FindElement(_locator);
-            }
-        }
-    }
-
+    private readonly IWebDriver _driver;
+    private readonly By _locator;
+    private readonly WebDriverWait _wait;
+    private readonly TimeSpan _timeout = TimeSpan.FromSeconds(20);
 
     public PageElement(IWebDriver driver, By locator)
     {
@@ -37,104 +20,133 @@ public class PageElement : IWebElement
         _wait = new WebDriverWait(_driver, _timeout);
     }
 
-    public string TagName => _element.TagName;
-    public string Text => _element.Text;
-    public bool Enabled => _element.Enabled;
-    public bool Selected => _element.Selected;
-    public Point Location => _element.Location;
-    public Size Size => _element.Size;
-    public bool Displayed => _element.Displayed;
-    public void Clear() => _element.Clear();
-    public IWebElement FindElement(By by) => _element.FindElement(by);
-    public ReadOnlyCollection<IWebElement> FindElements(By by) => _element.FindElements(by);
-    public List<PageElement> GetAllElements() => elements.Select(e => new PageElement(_driver, _locator)).ToList();
-    public string GetAttribute(string attributeName) => _element.GetAttribute(attributeName);
-    public string GetCssValue(string propertyName) => _element.GetCssValue(propertyName);
-    public string GetDomAttribute(string attributeName) => _element.GetDomAttribute(attributeName);
-    public string GetDomProperty(string propertyName) => _element.GetDomProperty(propertyName);
-    public void Submit() => _element.Submit();
-    public ISearchContext GetShadowRoot() => _element.GetShadowRoot();
+    private IWebElement GetElement()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            try
+            {
+                var element = _driver.FindElement(_locator);
+                if (element.Displayed || element.Enabled)
+                    return element;
+            }
+            catch (StaleElementReferenceException) { }
+            catch (NoSuchElementException) { }
+            Thread.Sleep(500);
+        }
+        throw new NoSuchElementException($"Element '{_locator}' not found or not stable after retries.");
+    }
+
+    private bool IsElementInteractable(IWebElement element) => element != null && element.Displayed && element.Enabled;
+    public string TagName => GetElement().TagName;
+    public string Text => GetElement().Text;
+    public bool Enabled => GetElement().Enabled;
+    public bool Selected => GetElement().Selected;
+    public Point Location => GetElement().Location;
+    public Size Size => GetElement().Size;
+    public bool Displayed => GetElement().Displayed;
+    public void Clear() => GetElement().Clear();
+    public IWebElement FindElement(By by) => GetElement().FindElement(by);
+    public ReadOnlyCollection<IWebElement> FindElements(By by) => GetElement().FindElements(by);
+    public string GetAttribute(string name) => GetElement().GetAttribute(name);
+    public string GetCssValue(string name) => GetElement().GetCssValue(name);
+    public string GetDomAttribute(string name) => GetElement().GetDomAttribute(name);
+    public string GetDomProperty(string name) => GetElement().GetDomProperty(name);
+    public void Submit() => GetElement().Submit();
+    public ISearchContext GetShadowRoot() => GetElement().GetShadowRoot();
     public void WaitForClickability() => _wait.Until(ExpectedConditions.ElementToBeClickable(_locator));
     public void WaitForVisibility() => _wait.Until(ExpectedConditions.ElementIsVisible(_locator));
-    public void SelectByText(string text) => new SelectElement(_element).SelectByText(text);
-    public void SelectByValue(string value) => new SelectElement(_element).SelectByValue(value);
-    public void SelectByIndex(int index) => new SelectElement(_element).SelectByIndex(index);
-    public void DeselectAll() => new SelectElement(_element).DeselectAll();
-    public void DoubleClick() => new Actions(_driver).DoubleClick(_element).Perform();
-    public void Hover() => new Actions(_driver).MoveToElement(_element).Perform();
-    public void RightClick() => new Actions(_driver).ContextClick(_element).Perform();
-    public void DragAndDrop(PageElement source, PageElement target) => new Actions(_driver).DragAndDrop(source._element, target._element).Perform();
-    public void ActionClick() => new Actions(_driver).MoveToElement(_element).Click().Perform();
-    bool IsElementInteractable(IWebElement element) => element != null && element.Displayed && element.Enabled;
+    public void SelectByText(string text) => new SelectElement(GetElement()).SelectByText(text);
+    public void SelectByValue(string value) => new SelectElement(GetElement()).SelectByValue(value);
+    public void SelectByIndex(int index) => new SelectElement(GetElement()).SelectByIndex(index);
+    public void DeselectAll() => new SelectElement(GetElement()).DeselectAll();
+    public void DoubleClick() => new Actions(_driver).DoubleClick(GetElement()).Perform();
+    public void Hover() => new Actions(_driver).MoveToElement(GetElement()).Perform();
+    public void RightClick() => new Actions(_driver).ContextClick(GetElement()).Perform();
+    public void ActionClick() => new Actions(_driver).MoveToElement(GetElement()).Click().Perform();
+
+    public void DragAndDrop(PageElement source, PageElement target)
+    {
+        new Actions(_driver)
+            .DragAndDrop(source.GetElement(), target.GetElement())
+            .Perform();
+    }
 
     public void Click()
     {
         try
         {
-            if (IsElementInteractable(_element))
+            if (IsElementInteractable(GetElement()))
             {
-                _element.Click();
+                GetElement().Click();
             }
             else
             {
                 WaitForClickability();
-                _element.Click();
+                GetElement().Click();
             }
         }
         catch (ElementClickInterceptedException)
         {
             WaitForLoadingIconToDisappear();
-            _element.Click();
+            GetElement().Click();
         }
         catch (ElementNotInteractableException)
         {
             WaitForLoadingIconToDisappear();
-            _element.Click();
+            GetElement().Click();
         }
     }
 
+
     public void SendKeys(string text)
     {
+        var element = GetElement();
         try
         {
-            
-            if (IsElementInteractable(_element))
+            if (IsElementInteractable(element))
             {
-                _element.SendKeys(text);
+                element.SendKeys(text);
             }
             else
             {
                 WaitForLoadingIconToDisappear();
-                _element.SendKeys(text);
+                GetElement().SendKeys(text);
             }
         }
         catch (ElementNotInteractableException)
         {
             WaitForLoadingIconToDisappear();
-            _element.Click();
+            GetElement().SendKeys(text);
         }
     }
 
     public void JSClick()
     {
-        if (IsElementInteractable(_element))
+        var element = GetElement();
+        if (IsElementInteractable(element))
         {
             WaitForClickability();
-            ((IJavaScriptExecutor)_driver).ExecuteScript("arguments[0].click();", _element);
+            ((IJavaScriptExecutor)_driver).ExecuteScript("arguments[0].click();", element);
         }
         else
-            throw new ElementNotInteractableException("The element is not interactable.");
+        {
+            throw new ElementNotInteractableException("JSClick: Element not interactable.");
+        }
     }
 
     public void ClearAndSendKeys(string text)
     {
-        if (IsElementInteractable(_element))
+        var element = GetElement();
+        if (IsElementInteractable(element))
         {
-            _element.Clear();
-            _element.SendKeys(text);
+            element.Clear();
+            element.SendKeys(text);
         }
         else
-            throw new ElementNotInteractableException("The element is not interactable.");
+        {
+            throw new ElementNotInteractableException("ClearAndSendKeys: Element not interactable.");
+        }
     }
 
     public bool ElementExists()
@@ -143,11 +155,7 @@ public class PageElement : IWebElement
         {
             return _driver.FindElements(_locator).Count > 0;
         }
-        catch (NoSuchElementException)
-        {
-            return false;
-        }
-        catch (StaleElementReferenceException)
+        catch
         {
             return false;
         }
@@ -155,32 +163,36 @@ public class PageElement : IWebElement
 
     public void ScrollIntoView()
     {
-        if (IsElementInteractable(_element))
-            ((IJavaScriptExecutor)_driver).ExecuteScript("arguments[0].scrollIntoView(true);", _element);
-        else
-            throw new ElementNotInteractableException("The element is not interactable.");
+        var element = GetElement();
+        if (IsElementInteractable(element))
+        {
+            ((IJavaScriptExecutor)_driver).ExecuteScript("arguments[0].scrollIntoView(true);", element);
+        }
     }
 
     public void SendKeysByJavascript(string text)
     {
-        if (IsElementInteractable(_element))
-            ((IJavaScriptExecutor)_driver).ExecuteScript("arguments[0].value = arguments[1];", _element, text);
-        else
-            throw new ElementNotInteractableException("The element is not interactable.");
+        var element = GetElement();
+        if (IsElementInteractable(element))
+        {
+            ((IJavaScriptExecutor)_driver).ExecuteScript("arguments[0].value = arguments[1];", element, text);
+        }
     }
 
     public void FocusOnElement()
     {
-        if (IsElementInteractable(_element))
-            ((IJavaScriptExecutor)_driver).ExecuteScript("arguments[0].focus();", _element);
-        else
-            throw new ElementNotInteractableException("The element is not interactable.");
+        var element = GetElement();
+        if (IsElementInteractable(element))
+        {
+            ((IJavaScriptExecutor)_driver).ExecuteScript("arguments[0].focus();", element);
+        }
     }
 
-    void WaitForLoadingIconToDisappear()
+    private void WaitForLoadingIconToDisappear()
     {
         By loadingIconLocator = By.XPath(ConfigurationManager.LoadingIconXpath);
-        if (_driver.FindElements(loadingIconLocator).FirstOrDefault(e => e.Displayed) != null)
+        var loadingIcon = _driver.FindElements(loadingIconLocator).FirstOrDefault(e => e.Displayed);
+        if (loadingIcon != null)
         {
             _wait.Until(ExpectedConditions.InvisibilityOfElementLocated(loadingIconLocator));
         }
@@ -188,38 +200,34 @@ public class PageElement : IWebElement
 
     public void SetCheckbox(bool check)
     {
-        if (_element.Selected != check)
+        var element = GetElement();
+        if (element.Selected != check)
         {
-            _element.Click();
+            element.Click();
         }
     }
 
-    public async Task EnterTextWithDelayAsync(string text, int delayInMilliseconds = 100)
+    public async Task EnterTextWithDelayAsync(string text, int delayInMs = 100)
     {
-        if (IsElementInteractable(_element))
+        var element = GetElement();
+        if (IsElementInteractable(element))
         {
-            _element.Clear();
+            element.Clear();
             foreach (char c in text)
             {
-                _element.SendKeys(c.ToString());
-                await Task.Delay(delayInMilliseconds);
+                element.SendKeys(c.ToString());
+                await Task.Delay(delayInMs);
             }
         }
-        else
-            throw new ElementNotInteractableException($"Cannot enter text on the element located by '{_locator}' on page '{_driver.Url}'.");
     }
 
     public bool IsDisplayed()
     {
         try
         {
-            return _element != null && _element.Displayed;
+            return GetElement().Displayed;
         }
-        catch (NoSuchElementException)
-        {
-            return false;
-        }
-        catch (StaleElementReferenceException)
+        catch
         {
             return false;
         }
@@ -227,28 +235,30 @@ public class PageElement : IWebElement
 
     public void SelectAllTextBeforeSendKey(string text)
     {
-        _element.Click();
-        _element.SendKeys(Keys.Control + "a");
-        _element.SendKeys(Keys.Backspace);
-        _element.SendKeys(text);
+        var element = GetElement();
+        element.Click();
+        element.SendKeys(Keys.Control + "a");
+        element.SendKeys(Keys.Backspace);
+        element.SendKeys(text);
     }
 
     public async Task<IWebElement> FindElementWithRetryAsync(int retryCount = 3, int delayInSeconds = 1)
     {
-        IWebElement element = null;
         for (int i = 0; i < retryCount; i++)
         {
             try
             {
-                element = _element;
-                break;
+                return GetElement();
             }
-            catch (NoSuchElementException)
+            catch
             {
                 if (i == retryCount - 1) throw;
                 await Task.Delay(TimeSpan.FromSeconds(delayInSeconds));
             }
         }
-        return element;
+        throw new NoSuchElementException($"Element '{_locator}' not found after retry.");
     }
+
+    public List<PageElement> GetAllElements() =>
+        _driver.FindElements(_locator).Select(e => new PageElement(_driver, _locator)).ToList();
 }
